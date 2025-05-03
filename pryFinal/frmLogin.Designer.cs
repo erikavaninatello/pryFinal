b7a2cbb52e0b81598fb4ec7d4d2815ff263abe2e
erikavaninatello <vaninatello9@gmail.com>
2025-05-03 18:20:26 -0300

    Agregar archivos de proyecto.

---
+++ pryFinal/frmLogin.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace pryFinal
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.panelPrinc = new Guna.UI2.WinForms.Guna2Panel();
+            this.btnLogin = new Guna.UI2.WinForms.Guna2Button();
+            this.lblRegistrarse = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.txtUsuario = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtClave = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lblSaludo = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblMensaje = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.principal = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.logo = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.panelPrinc.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.principal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelPrinc
+            // 
+            this.panelPrinc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(43)))));
+            this.panelPrinc.Controls.Add(this.lblMensaje);
+            this.panelPrinc.Controls.Add(this.lblSaludo);
+            this.panelPrinc.Controls.Add(this.btnLogin);
+            this.panelPrinc.Controls.Add(this.lblRegistrarse);
+            this.panelPrinc.Controls.Add(this.txtUsuario);
+            this.panelPrinc.Controls.Add(this.txtClave);
+            this.panelPrinc.Controls.Add(this.logo);
+            this.panelPrinc.Location = new System.Drawing.Point(-5, -6);
+            this.panelPrinc.Name = "panelPrinc";
+            this.panelPrinc.Size = new System.Drawing.Size(261, 457);
+            this.panelPrinc.TabIndex = 0;
+            this.panelPrinc.Paint += new System.Windows.Forms.PaintEventHandler(this.panelPrinc_Paint);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BorderRadius = 10;
+            this.btnLogin.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnLogin.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnLogin.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnLogin.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnLogin.FillColor = System.Drawing.Color.MediumVioletRed;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(69, 337);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(104, 45);
+            this.btnLogin.TabIndex = 7;
+            this.btnLogin.Text = "Iniciar Sesion";
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblRegistrarse
+            // 
+            this.lblRegistrarse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblRegistrarse.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblRegistrarse.Location = new System.Drawing.Point(156, 300);
+            this.lblRegistrarse.Name = "lblRegistrarse";
+            this.lblRegistrarse.Size = new System.Drawing.Size(56, 15);
+            this.lblRegistrarse.TabIndex = 10;
+            this.lblRegistrarse.Text = "Registrarse";
+            this.lblRegistrarse.Click += new System.EventHandler(this.lblRegistrarse_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(153)))), ((int)(((byte)(149)))));
+            this.txtUsuario.BorderRadius = 6;
+            this.txtUsuario.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtUsuario.DefaultText = "";
+            this.txtUsuario.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtUsuario.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtUsuario.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtUsuario.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtUsuario.FillColor = System.Drawing.Color.Black;
+            this.txtUsuario.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtUsuario.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtUsuario.Location = new System.Drawing.Point(27, 186);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.PlaceholderText = "Usuario";
+            this.txtUsuario.SelectedText = "";
+            this.txtUsuario.Size = new System.Drawing.Size(200, 36);
+            this.txtUsuario.TabIndex = 8;
+            // 
+            // txtClave
+            // 
+            this.txtClave.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(153)))), ((int)(((byte)(149)))));
+            this.txtClave.BorderRadius = 6;
+            this.txtClave.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtClave.DefaultText = "";
+            this.txtClave.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtClave.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtClave.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtClave.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtClave.FillColor = System.Drawing.Color.Black;
+            this.txtClave.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtClave.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtClave.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtClave.Location = new System.Drawing.Point(27, 246);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.PlaceholderText = "Contraseña";
+            this.txtClave.SelectedText = "";
+            this.txtClave.Size = new System.Drawing.Size(200, 36);
+            this.txtClave.TabIndex = 9;
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.BackColor = System.Drawing.Color.Transparent;
+            this.lblSaludo.Font = new System.Drawing.Font("Century Gothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaludo.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblSaludo.Location = new System.Drawing.Point(87, 137);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(86, 23);
+            this.lblSaludo.TabIndex = 71;
+            this.lblSaludo.Text = "Hola, Astro";
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.BackColor = System.Drawing.Color.Transparent;
+            this.lblMensaje.ForeColor = System.Drawing.Color.LawnGreen;
+            this.lblMensaje.Location = new System.Drawing.Point(124, 409);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(12, 15);
+            this.lblMensaje.TabIndex = 72;
+            this.lblMensaje.Text = "...";
+            // 
+            // principal
+            // 
+            this.principal.Image = global::pryFinal.Properties.Resources.Fondo_principal;
+            this.principal.ImageRotate = 0F;
+            this.principal.Location = new System.Drawing.Point(254, -6);
+            this.principal.Name = "principal";
+            this.principal.Size = new System.Drawing.Size(486, 492);
+            this.principal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.principal.TabIndex = 1;
+            this.principal.TabStop = false;
+            // 
+            // logo
+            // 
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.ImageRotate = 0F;
+            this.logo.Location = new System.Drawing.Point(87, 55);
+            this.logo.Name = "logo";
+            this.logo.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.logo.Size = new System.Drawing.Size(74, 76);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logo.TabIndex = 1;
+            this.logo.TabStop = false;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 450);
+            this.Controls.Add(this.principal);
+            this.Controls.Add(this.panelPrinc);
+            this.Name = "frmLogin";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.panelPrinc.ResumeLayout(false);
+            this.panelPrinc.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.principal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel panelPrinc;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox logo;
+        private Guna.UI2.WinForms.Guna2PictureBox principal;
+        private Guna.UI2.WinForms.Guna2Button btnLogin;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblRegistrarse;
+        private Guna.UI2.WinForms.Guna2TextBox txtUsuario;
+        private Guna.UI2.WinForms.Guna2TextBox txtClave;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblSaludo;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblMensaje;
+    }
+}
+
---
+++ pryFinal/frmLogin.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Guna.UI2.WinForms;
+
+namespace pryFinal
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            frmAddUser frmAddUser= new frmAddUser();
+            frmAddUser.Show();
+            this.Hide();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text.Trim();
+            string clave = txtClave.Text;
+
+            var usuarios = clsUserManager.CargarUsuarios();
+
+           
+            var usuarioExistente = usuarios.FirstOrDefault(u => u.Usuario == usuario);
+
+            if (usuarioExistente == null)
+            {
+                lblMensaje.Text = "El usuario no existe.";
+                return;
+            }
+
+        
+            if (usuarioExistente.Bloqueado)
+            {
+                lblMensaje.Text = "El usuario está bloqueado.";
+                return;
+            }
+
+         
+            if (usuarioExistente.Clave != clave)
+            {
+                usuarioExistente.IntentosFallidos++;
+
+                if (usuarioExistente.IntentosFallidos >= 3)
+                {
+                    usuarioExistente.Bloqueado = true;
+                    lblMensaje.Text = "Usuario bloqueado por intentos fallidos.";
+                }
+                else
+                {
+                    lblMensaje.Text = "Contraseña incorrecta. Intentos fallidos: " + usuarioExistente.IntentosFallidos;
+                }
+
+                clsUserManager.GuardarUsuarios(usuarios);
+                return;
+            }
+
+         
+            usuarioExistente.IntentosFallidos = 0;
+            clsUserManager.GuardarUsuarios(usuarios);
+
+
+            frmMain frm = new frmMain();
+            frm.Show();
+            this.Hide();  
+        }
+
+        private void lblRegistrarse_Click(object sender, EventArgs e)
+        {
+            new frmAddUser().ShowDialog();
+        }
+
+        private void panelPrinc_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
